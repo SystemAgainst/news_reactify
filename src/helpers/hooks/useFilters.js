8d99f4ad0843d17221de8ea944d01737faee34bf
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-09-21 20:46:59 +1000

    ref: remove unused constant

--- src/helpers/hooks/useFilters.js
+++ src/helpers/hooks/useFilters.js
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { PAGE_SIZE } from '../../constants/constants.js';
 
 export const useFilters = (initialFilters) => {
   const [filters, setFilters] = useState({initialFilters});
