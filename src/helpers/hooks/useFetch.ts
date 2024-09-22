501475e7f9f228e088dd9069ea03baccac1a93db
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-09-22 20:46:07 +1000

    typed useFetch

--- src/helpers/hooks/useFetch.ts
+++ src/helpers/hooks/useFetch.ts
@@ -1,9 +1,19 @@
 import { useEffect, useState } from 'react';
 
-export const useFetch = (fetchFunc, params) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+interface FetchFunction<P, T> {
+  (params?: P): Promise<T>;
+}
+
+interface IUseFetchResult<T> {
+  data: T | null | undefined;
+  isLoading: boolean;
+  error?: Error | null;
+}
+
+export const useFetch = <T, P>(fetchFunc: FetchFunction<P, T>, params?: P): IUseFetchResult<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
 
   const paramsToStr = params ? new URLSearchParams(params).toString() : '';
 
@@ -18,7 +28,7 @@ export const useFetch = (fetchFunc, params) => {
 
         setData(result);
       } catch (e) {
-        setError(e);
+        setError(e as Error);
       } finally {
         setIsLoading(false);
       }
