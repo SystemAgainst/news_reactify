edd7f82e31f75912d07f13049985e7439f1cd1f0
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-10-11 11:16:25 +1000

    add middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import newsReducer from './slices/newsSlice.ts';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { newsApi } from './services/newsApi.ts';
 
 export const store = configureStore({
   reducer: {
     news: newsReducer,
+    [newsApi.reducerPath]: newsApi.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(newsApi.middleware)
 });
 
 export type RootState = ReturnType<typeof store.getState>;
